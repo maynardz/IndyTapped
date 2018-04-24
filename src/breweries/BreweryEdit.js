cb2e6b7f532984ce8f9cfa62970fd2c21484a46c
maynardz <maynard.zach23@gmail.com>
2018-04-24 16:40:54 -0400

    client side

---
+++ src/breweries/BreweryEdit.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Button, Form, FormGroup, Input, Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+class BreweryEdit extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: '',
+            brewery: '',
+            rating: ''
+        };
+    }
+
+    componentWillMount() {
+        this.setState({
+            id: this.props.userbreweries.id,
+            brewery: this.props.userbreweries.Brewery,
+            rating: this.props.userbreweries.Rating,
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.update(event, this.state)
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <Modal isOpen={true} >
+                    <ModalHeader >Edit a Brewery</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleSubmit} >
+                            <FormGroup>
+                                <Input id="brewery" type="text" name="brewery" value={this.state.Brewery} placeholder="Enter a brewery" onChange={this.handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="select" name="rating" id="rating" value={this.state.Rating} onChange={this.handleChange} placeholder="Type">
+                                    <option>Rating</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Input>
+                            </FormGroup>
+                            <Button type="submit" color="secondary"> Submit </Button>
+                        </Form>
+                    </ModalBody>
+
+                </Modal>
+
+            </div>
+        )
+    }
+}
+
+
+export default BreweryEdit;
---
+++ src/breweries/BreweryResults.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Results = styled.ul`
+overflow: scroll;
+height: 30em;
+width: 18.56em;
+color: white;
+font-size: 17px;
+font-family: 'Raleway', sans-serif;
+::-webkit-scrollbar { 
+    display: none; 
+}
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+    height: 20em;
+}
+@media only screen and (min-width : 321px) {
+    height: 23em;
+    }
+`;
+
+const BreweryResults = ({ results }) => {
+    const resultsFormatted = results.map((result) =>
+        <li key={result}>
+            <div>
+                <p>{result.Brewery}</p>
+                <p>{result.City}{', '}{result.State}{' - '}{' # '}{result.id}</p>
+            </div>
+        </li>
+    );
+    return (
+        <Results id="results">
+            {resultsFormatted}
+        </Results>
+    );
+}
+export default BreweryResults;
