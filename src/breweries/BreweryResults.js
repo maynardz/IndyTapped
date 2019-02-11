1d66c10a1724109de0fe04c8fc72310ee4a00c2a
Zach Maynard <maynard.zach23@gmail.com>
2019-02-11 13:09:47 -0500

    bold font/table color

--- src/breweries/BreweryResults.js
+++ src/breweries/BreweryResults.js
@@ -4,26 +4,34 @@ import styled from 'styled-components';
 const Results = styled.ul`
 overflow: scroll;
 height: 30em;
-width: 18.56em;
-color: white;
+width: 34.25em;
+margin-top: 1em;
+border-radius: 5px;
+color: black;
+background-color: white;
 font-size: 17px;
 font-family: 'Raleway', sans-serif;
 ::-webkit-scrollbar { 
     display: none; 
 }
 @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
-    height: 20em;
+    height: 18em;
+    width: 13em;
 }
 @media only screen and (min-width : 321px) {
-    height: 23em;
+    height: 20em;
     }
 `;
+
+const Bold = styled.p`
+font-weight: bold;
+`
 
 const BreweryResults = ({ results }) => {
     const resultsFormatted = results.map((result) =>
         <li key={result}>
             <div>
-                <p>{result.Brewery}</p>
+                <Bold>{result.Brewery}</Bold>
                 <p>{result.Address}</p>
                 <p>{result.City}{', '}{result.State}{' '}{result.ZipCode}</p>
             </div>
